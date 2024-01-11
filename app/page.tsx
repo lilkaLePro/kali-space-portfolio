e49ca2aac36c35e70a1dacf625096e16f68cdfc7
lilkaLePro <kaly100diallo@gmail.com>
2024-01-11 17:38:42 +0100

    add text

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
 
 export default function Home() {
-  return <div> ma page principale</div>
+  return <div> ma page principale et je compte faire un blog plus tard</div>
 }
